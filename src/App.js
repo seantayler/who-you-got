b3a9ca3a3c95b2d4e45a8d029e6f043f9530e247
Sean Tayler <seantayler@gAM7acd39.local>
2019-07-12 14:04:05 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+
+  }
+
+  render(){
+
+    const playersList = data.map((player, i) => {
+      return <option>{player.name}</option>
+    })
+
+    return (
+      <div className="App">
+  
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    { name: 'Lebron James', appg: 32, per: 7, pm: 6 },
+    { name: 'Michael Jordan', appg: 32, per: 7, pm: 6 },
+    { name: 'Kobe Bryant', appg: 32, per: 7, pm: 6 },
+    { name: 'Shaquille Oneal', appg: 32, per: 7, pm: 6 },
+    { name: 'Kevin Durant', appg: 32, per: 7, pm: 6 },
+    { name: 'Steph Curry', appg: 32, per: 7, pm: 6 },
+    { name: 'Kareem Abdul Jabaar', appg: 32, per: 7, pm: 6 },
+    { name: 'Magic Johnson', appg: 32, per: 7, pm: 6 },
+    { name: 'Larry Bird', appg: 32, per: 7, pm: 6 },
+    { name: 'Tim Duncan', appg: 32, per: 7, pm: 6 },
+]
